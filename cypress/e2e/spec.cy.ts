70dd5aa62b06d908210e409af5b59a1bdf1c192b
JonR205 <gohan205@gmail.com>
2024-07-21 22:48:43 -0500

    added cypress file reference

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types= "cypress" />
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+  });
+});
